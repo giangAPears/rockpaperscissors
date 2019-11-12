41f25ebbd25e26367d9ce1ccb859cca54320792f
Jessica Giang <giang@aclanyc.com>
2019-11-12 23:22:49 +0000

    display user input

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-/* global $ */
-
-
+$("document").ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+    console.log("hello");
+    //GLOBAL VARIABLES
+    /* global $ */
+    $("#shoot").click(function() {
+        let userChoice = $("#input").val();
+        let lowerCaseUserChoice = userChoice.toLowerCase();
+        
+    });
+});
 
 // DOCUMENT READY FUNCTION BELOW
 
