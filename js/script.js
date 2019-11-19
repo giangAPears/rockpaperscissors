632b3b7ed21708363dbe992b7f0412acd7249886
Jessica Giang <giang@aclanyc.com>
2019-11-19 23:13:12 +0000

    printed user input and computer input

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 $("document").ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-    console.log("hello");
+    alert("hello world");
     //GLOBAL VARIABLES
     /* global $ */
     $("#shoot").click(function() {
-        let userChoice = $("#input").val();
-        let lowerCaseUserChoice = userChoice.toLowerCase();
-        
+
+        //stores user choice
+        let userInput = $("#input").val(); 
+        //makes input ower case so if else statement is easier to write
+        let lowerCaseUserChoice = userInput.toLowerCase();
+        $("#userChoice").text(lowerCaseUserChoice);
+
+        let potentialResults = ["Rock", "Paper", "Scissors"]; 
+        let randomNumber = Math.floor(Math.random() * 3);
+        let computerResult = potentialResults[randomNumber];
+        $("#computerChoice").text(computerResult);
+
+        if(lowerCaseUserChoice===computerResult){
+
+        }
     });
 });
 
